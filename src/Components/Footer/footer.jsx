d025a2347e01c9885ac26bfa4156a4e72c2af1b8
lecovska <lecovska@gmail.com>
2022-12-14 13:04:54 +0100

    editing footer

--- src/Components/Footer/footer.jsx
+++ src/Components/Footer/footer.jsx
@@ -1,38 +1,49 @@
 import React from "react";
 import "./footer.css";
 import footerLogo from "../../Images/logoPng.png";
+import locationLogo from "../../Images/Location-2---icon.png";
+import emailLogo from "../../Images/e-mail---icon.png";
+import phoneLogo from "../../Images/output-onlinepngtools.png";
 
-function Footer(){
-    return(
+function Footer() {
+    return (
         <div className="footer">
             <div className="info">
-                    <div className="logo-pic"><img className="footerLogo" src={footerLogo} alt="footerLogo"></img></div>
+                <div className="main-logo"><img className="footerLogo" src={footerLogo} alt="footerLogo"></img></div>
+                <div className="info-wrapper">
+                    <div className="logo-pic">
+                        <img id="locationLogo" src={locationLogo} alt="locationLogo"></img>
+                        <img id="emailLogo" src={emailLogo} alt="emailLogo"></img>
+                        <img id="phoneIcon" src={phoneLogo} alt="phoneIcone"></img>
+                    </div>
+
                     <div className="kontakt-info">
-                    <p>lokacija</p>
-                    <p>email</p>
-                    <p>telefon</p>
+                        <p>Beograd</p>
+                        <p>office@flex-kurir.rs</p>
+                        <p>+381 66 624.29.59</p>
+                    </div>
                 </div>
             </div>
             <div className="usluge">
-                <h3>Usluge</h3>
-                <p>dostava</p>
-                <p>dostava</p>
-                <p>dostava</p>
-                <p>dostava</p>
-                <p>dostava</p>
+                <h3 className="h3-footer">Usluge</h3>
+                <a  href="nesto">Dostava Beograd</a>
+                <a href="nesto">Danas za danas</a>
+                <a href="nesto">Danas za sutra</a>
+                <a href="nesto">B2C dostava</a>
+                <a href="nesto">B2B dostava</a>
             </div>
             <div className="informacije">
-            <h3>Informacije</h3>
-                <p>pitanja</p>
-                <p>cenovnik</p>
-                <p>uslovi</p>
-                <p>zakazivanje</p>
+                <h3 className="h3-footer">Korisne informacije</h3>
+                <a href="nesto">Najčešća pitanja</a>
+                <a href="nesto">Cenovnik</a>
+                <a href="nesto">Uslovi korišćenja</a>
+                <a href="nesto">Zakazivanje kurira</a>
             </div>
-            <div className="kontakt">
 
-                <h3>KONTAKTIRAJTE NAS!</h3>
-                <p>Tu smo za vasa pitanja</p>
-                <button>KONTAKT</button>
+            <div className="kontakt">
+                <h3 className="h3-footer">KONTAKTIRAJTE NAS!</h3>
+                <p id="kontakt-p">Tu smo za Vaša pitanja.</p>
+                <button className="kontakt-btn">KONTAKT</button>
             </div>
 
         </div>
