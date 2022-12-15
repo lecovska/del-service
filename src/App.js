79d0402f6bbc3360ee82ab06af8e5607a41bb570
Isidora Djordjevic <isidora.djordjevic1998@gmail.com>
2022-12-15 18:56:39 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,25 +2,27 @@ import React from 'react';
 // import './App.css';
 //import HomePage from './Pages/HomePage/homePage';
 // import Cenovnik from './Pages/CenovnikPage/cenovnikPage';
-// import Kontakt from './Pages/Kontakt/kontakt';
+import Kontakt from './Pages/Kontakt/kontakt';
 // import NovostiSinglePage from './Pages/NovostiSinglePage/novostiSinglePage';
 // import NovostiMainPage from './Pages/NovostiMainPage/NovostiMainPage';
 // import Pitanja from "./Pages/Pitanja/pitanja";
 //  import About from './Pages/About/about';
-import Zakazi from './Pages/Zakazi/zakazi';
+//import Zakazi from './Pages/Zakazi/zakazi';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
     {/* <HomePage />  */}
       {/* <Cenovnik /> 
-      <Kontakt /> 
+       
+       <Zakazi/>
       <NovostiSinglePage /> 
      <NovostiMainPage /> 
       <Pitanja/>   */}
        {/* <About/> */}
-      <Zakazi/>
+      <Kontakt/>
     </div>
   );
 }
