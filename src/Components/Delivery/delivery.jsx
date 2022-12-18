6ec571422faa3914bfa6543822d13d51601331d3
lecovska <lecovska@gmail.com>
2022-12-18 15:42:55 +0100

    edit HomePage

--- src/Components/Delivery/delivery.jsx
+++ src/Components/Delivery/delivery.jsx
@@ -7,23 +7,22 @@ function Delivery() {
     return (
         <div className="delivery">
 
-            <h1>FLEX,<br></br>Najbolja kurirska sluzba u gradu</h1>
+            <h1 id="h1-delivery">FLEX,<br></br>Najbolja kurirska sluzba u gradu</h1>
             <div className="delivery-flex">
-                <div className="delivery-img"><img className="deliveryImg" src={slika1} alt="slika1"></img></div>
-
+                <div className="delivery-img"></div>
+                {/* <img className="deliveryImg" src={slika1} alt="slika1"></img> */}
+                {/* <img className="deliveryImg1" src={slika2} alt="slika2"> </img>*/}
                 <div className="delivery-text">
                     <h3>Dostava Beograd</h3>
-                    <p>Pozovite nas da preuzmemo Vašu pošiljku danas.</p>
-                    <p>Primaoc može da očekuje paket odmah ili najkasnije do 18h.</p>
+                    <p>Pozovite nas da preuzmemo Vašu pošiljku danas.Primaoc može da očekuje paket odmah ili najkasnije do 18h.</p>
                     <a className="delivery-a" href="nesto">Saznaj više</a>
                 </div>
 
-                <div className="delivery-img"><img className="deliveryImg1" src={slika2} alt="slika2"></img></div>
+                <div className="delivery-img2"></div>
 
                 <div className="delivery-text">
                     <h3>Dostava danas za danas</h3>
-                    <p>Za one koji nemaju vremena tu je dostava. Danas za DANAS.</p>
-                    <p>Vašu pošiljku dostavljamo na adresu primaoca odmah.</p>
+                    <p>Za one koji nemaju vremena tu je dostava. Danas za DANAS. Vašu pošiljku dostavljamo na adresu primaoca odmah.</p>
                     <a className="delivery-a" href="nesto">Saznaj više</a>
                 </div>
             </div>
