12e486d42cf5ab86e6246079fccf43f11f5a7e93
Isidora Djordjevic <isidora.djordjevic1998@gmail.com>
2022-12-21 19:28:53 +0100

    Update about.jsx

--- src/Pages/About/about.jsx
+++ src/Pages/About/about.jsx
@@ -17,7 +17,7 @@ function About() {
             <div className="image-about">
                 <div className="greeting">
                     <span className="main-title">O nama</span>
-                    <a href="/src/Pages/homepageto"><img id="homeIcon" src={homeIcon} alt="homeIcon"></img> Početna - Flex kurirska služba</a>
+                    <a href="/src/Pages/homepage"><img id="homeIcon" src={homeIcon} alt="homeIcon"></img> Početna - Flex kurirska služba</a>
                 </div>
             </div>
             <div className="small-wrapper">
