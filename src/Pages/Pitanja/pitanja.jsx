7fdb8e7171a10064207dc3f763c8384ff96ccd33
Isidora Djordjevic <isidora.djordjevic1998@gmail.com>
2022-12-24 15:52:46 +0100

    Update pitanja.jsx

--- src/Pages/Pitanja/pitanja.jsx
+++ src/Pages/Pitanja/pitanja.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import "./pitanja.css";
 import Header from "../../Components/Header/header";
 import Footer from "../../Components/Footer/footer";
+import homeIcon from "../../Images/homeIcon.png";
 
 function Pitanja(){
     return(
@@ -9,9 +10,8 @@ function Pitanja(){
 <Header/>
 <div className="image">
                 <div className="greeting">
-                    <span className="main-title">Kontakt</span>
-                    <button>Pocetna - Flex kurirska sluzba</button>
-                </div>
+                    <span className="main-title">Pitanja</span>
+                    <a href="/"><img id="homeIcon" src={homeIcon} alt="homeIcon"></img> Početna - Flex kurirska služba</a>                </div>
             </div>
             <div className="questions">
 <h1>Vasa najcesca pitanja</h1>
