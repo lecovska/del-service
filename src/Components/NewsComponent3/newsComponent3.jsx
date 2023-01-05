519b7a49b514e8b68940134479f94ea9bd80f707
Isidora Djordjevic <isidora.djordjevic1998@gmail.com>
2023-01-05 13:25:02 +0100

    Update newsComponent3.jsx

--- src/Components/NewsComponent3/newsComponent3.jsx
+++ src/Components/NewsComponent3/newsComponent3.jsx
@@ -7,9 +7,8 @@ function NewsComponent3() {
             <div className="text">
                 <div className="pic-of-news3"></div>
                 <div className="text-wrapp">
-                    <h3>Flex, nova kurirska služba, koja štedi Vaše
-                        vreme</h3>
-                    <p className="newsSliderP">Poštovani klijenti,<br></br> kurirska služba za vreme praznika neće raditi sledećim danima:<br></br>
+                    <h3>Neradni dani:</h3>
+                    <p className="newsSliderP">Kurirska služba za vreme praznika neće raditi sledećim danima:<br></br>
                         Ponedeljak 02.01.2023.
                         Utorak 03.01.2023.
                         Petak 06.01.2023.</p>
